6a902df2795727b5da78bda77a240d45555e540a
Alex Gourlay <alexdgourlay@gmail.com>
2020-01-22 11:55:56 +0000

    Initial Commit

---
+++ src/Models/Property.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UK_Property_API.Models
+{
+    public class Property
+    {
+        [Key]
+        public String Postcodes { get; set; }
+
+        public String Area { get; set; }
+        public String District { get; set; }
+        public String Sector { get; set; }
+        public String Unit { get; set; }
+    }
+}
---
+++ src/Models/PropertyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace UK_Property_API.Models
+{
+    public class PropertyContext : DbContext
+    {
+        public PropertyContext(DbContextOptions<PropertyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Property> PropertyData { get; set; }
+    }
+}
