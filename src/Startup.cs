2a6581125f60419f7d069d60a6434e99589afc50
Alex Gourlay <alexdgourlay@gmail.com>
2020-01-27 21:23:26 +0000

    Update Startup.cs
    Added app developer page when environment is in development mode

--- src/Startup.cs
+++ src/Startup.cs
@@ -39,6 +39,11 @@ namespace UK_Property_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             // Use GraphiQl, GraphQL testing tool.
             app.UseGraphiQl();
 
